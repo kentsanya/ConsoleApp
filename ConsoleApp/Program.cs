51b7e702e4d649e485cd765e1a2f3f13b656f921
Oleksandr Vdovitchenko <kentsanya91@gmail.com>
2023-06-21 15:24:17 +0300

    Add project files.

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,39 @@
+﻿using System.IO.Pipes;
+using System.Text;
+
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Введення даних
+            Console.WriteLine("Input of number`s player");
+            int number = int.Parse(Console.ReadLine());
+            switch (number)
+            {
+                //значення нашої зміної (number)
+                case 4:
+                    Console.WriteLine("Jordan");
+                    break;
+                case 6:
+                    Console.WriteLine("Duncan");
+                    break;
+                case 8:
+                    Console.WriteLine("Carry");
+                    break;
+                case 10:
+                    Console.WriteLine("Lebron");
+                    break;
+                case 2:
+                    Console.WriteLine("O’Neal");
+                    break;
+                default:
+                    Console.WriteLine("Uknown PLAYER");
+                    break;
+            }
+            
+        }
+    }
+}
---
+++ Test.Logical/Animal.cs
@@ -0,0 +1,54 @@
+﻿namespace Test.Logical
+{
+
+    //INTERACTION (Method+field (class or abstract class))
+    public class Dog : Animal
+    {
+        public Dog(Toys toysconcret) : base(toysconcret)
+        {
+        }
+    }
+
+    //Realization (signature Method (interfaces))
+    interface IMove 
+    {
+        void Move();
+    }
+
+    //Association
+    public  class Animal:IMove
+    {
+        public People people;
+        public Toys toys;
+        public Species species;
+
+        public Animal(Toys toysconcret) 
+        {
+            //Composition
+            species = new Species();
+            //Agregation
+            toys =  toysconcret;
+        }
+
+        //Realization in the class
+        public void Move()
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    public class People 
+    {
+
+    }
+    public class Toys 
+    {
+
+    }
+    public class Species 
+    {
+
+    }
+
+
+}
